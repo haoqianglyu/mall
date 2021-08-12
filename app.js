59b4c6d41a7e97dcc15607fbaebb47b3409544eb
haoqianglyu <15951656032@163.com>
2021-08-12 09:56:33 +0800

    init project

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-// app.js
-App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+//Page Object
+Page({
+  data: {
+    
   },
-  globalData: {
-    userInfo: null
+  //options(Object)
+  onLoad: function(options) {
+    
+  },
+  onReady: function() {
+    
+  },
+  onShow: function() {
+    
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  },
+  onPageScroll: function() {
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item) {
+
   }
-})
+});
+  
