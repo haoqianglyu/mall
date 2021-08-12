2a36ebca52d77254fb720210a6f49b123d794e8d
haoqianglyu <15951656032@163.com>
2021-08-12 16:54:32 +0800

    add index floor

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
-//Page Object
-Page({
-  data: {
+wx-App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
     
   },
-  //options(Object)
-  onLoad: function(options) {
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
     
   },
-  onReady: function() {
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
     
   },
-  onShow: function() {
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
     
-  },
-  onHide: function() {
-
-  },
-  onUnload: function() {
-
-  },
-  onPullDownRefresh: function() {
-
-  },
-  onReachBottom: function() {
-
-  },
-  onShareAppMessage: function() {
-
-  },
-  onPageScroll: function() {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item) {
-
   }
-});
-  
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,11 @@ import { request } from "../../request/index.js";
 Page({
   data: {
     //轮播图数组
-    swiperList:[]
+    swiperList:[],
+    //导航数组
+    catesList:[],
+    //楼层数据
+    floorList:[]
   },
   //页面开始加载 就会触发
   onLoad: function(options) {
@@ -19,12 +23,37 @@ Page({
     //     })
     //   }
     // });
-      request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+      this.getSwiperList();
+      this.getCateList();
+      this.getFloorList();
+  },
+
+  //获取轮播图数据
+  getSwiperList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
       .then(result=>{
           this.setData({
           swiperList:result.data.message
         })
       })
-  }
+  },
+  //获取分类导航数据
+  getCateList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"})
+      .then(result=>{
+          this.setData({
+          catesList:result.data.message
+        })
+      })
+  },
+  //获取楼层数据
+  getFloorList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"})
+      .then(result=>{
+          this.setData({
+          floorList:result.data.message
+        })
+      })
+  },
 });
   
