ce219fb7a609b87a9346546a1f1367f15c8d89a1
haoqianglyu <15951656032@163.com>
2021-08-12 11:20:47 +0800

    add swiper

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,22 @@
 //Page Object
 Page({
   data: {
-    
+    //轮播图数组
+    swiperList:[]
   },
-  //options(Object)
+  //页面开始加载 就会触发
   onLoad: function(options) {
-    
-  },
-  onReady: function() {
-    
-  },
-  onShow: function() {
-    
-  },
-  onHide: function() {
-
-  },
-  onUnload: function() {
-
-  },
-  onPullDownRefresh: function() {
-
-  },
-  onReachBottom: function() {
-
-  },
-  onShareAppMessage: function() {
-
-  },
-  onPageScroll: function() {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item) {
-
+    // 1.发送异步请求获取轮播图数据
+    var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      method: 'GET',
+      success: (result) => {
+        this.setData({
+          swiperList:result.data.message
+        })
+      }
+    });
+      
   }
 });
   
